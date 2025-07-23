b01341e74c8a2f1666325b083aaccab01e512534
RG3322 <raghavsankhyaan@gmail.com>
2025-07-23 17:39:46 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false // The version for libs.plugins.kotlin.android is specified in the /gradle/libs.versions.toml file.
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.ksp) apply false
+
+
+
+
+}
