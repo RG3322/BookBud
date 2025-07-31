8ad741688453824024c066b236af5a5d823411b6
RG3322 <raghavsankhyaan@gmail.com>
2025-07-31 16:24:21 +0530

    added

--- app/src/main/java/com/fire/bookbuddy/presentation/Effects/ShimmerEffects.kt
+++ app/src/main/java/com/fire/bookbuddy/presentation/Effects/ShimmerEffects.kt
@@ -5,9 +5,25 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AnimateShimmer() {
@@ -42,5 +58,70 @@ fun AnimateShimmer() {
 
 @Composable
 fun ShimmerGridItem(brush: Brush) {
+    Column(
+        modifier = Modifier
+            .height(150.dp)
+            .padding(all = 10.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Spacer(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(brush)
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
 
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .size(16.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .fillMaxWidth(fraction = 0.9f)
+                    .background(brush)
+            )
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .size(16.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .fillMaxWidth(fraction = 0.9f)
+                    .background(brush)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true,showSystemUi = true)
+@Composable
+fun ShimmerGridItemPreview() {
+    ShimmerGridItem(brush = Brush.linearGradient(listOf(Color.LightGray,Color.White,Color.Blue)))
+}
+@Composable
+fun categoryShimmer(){
+    val shimmerColor = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f))
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            )
+        ), label = ""
+    )
+    val brush = Brush.linearGradient(
+        colors = shimmerColor,
+        start = androidx.compose.ui.geometry.Offset(10f, 10f),
+        end = androidx.compose.ui.geometry.Offset(translateAnim.value, translateAnim.value)
+    )
 }
