00aa344052adf04dd6d03f42211753efc6016ef8
RG3322 <raghavsankhyaan@gmail.com>
2025-08-19 17:17:37 +0530

    added

--- app/src/main/java/com/fire/bookbuddy/presentation/AllBooksByCategory/BookByCategoryScreen.kt
+++ app/src/main/java/com/fire/bookbuddy/presentation/AllBooksByCategory/BookByCategoryScreen.kt
@@ -1,7 +1,15 @@
 package com.fire.bookbuddy.presentation.AllBooksByCategory
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,6 +22,8 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.fire.bookbuddy.presentation.Effects.AnimateShimmer
+import com.fire.bookbuddy.presentation.UIcomponent.Bookcart
 import com.fire.bookbuddy.presentation.viewModel.ViewModel
 
 
@@ -43,7 +53,14 @@ fun  BooksByCategoryScreen(
     topBar = {
         TopAppBar(
             title = {
-                Text(text = category)
+                Text( category)},
+            navigationIcon = {
+
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Arrow Back")
+                }
             }
         )
 
@@ -51,6 +68,53 @@ fun  BooksByCategoryScreen(
 
     }
     ){
+        innerpadding ->
+        val res = viewModel.state.value
+        Column (modifier = Modifier.fillMaxSize()
+            .padding(innerpadding)){
+
+            when{
+                res.isLoading ->{
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        LazyColumn {
+                            items(10){
+                                AnimateShimmer()
+                            }
+                        }
+                    }
+                }
+                res.error.isNotEmpty()->{
+                    Text(text=res.error)
+                }
+
+
+                res.items.isNotEmpty() ->{
+                    Column(modifier = Modifier.fillMaxSize()) {
+
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+                            items(res.items){
+                                Bookcart(
+                                    imageURL = it.image,
+                                    title = it.bookName,
+                                    author = it.bookAuthor,
+                                    description = it.bookDescription,
+                                    bookUrl = it.bookUrl,
+                                    navController = navController
+                                )
+                            }
+
+                        }
+
+
+                    }
+                }
+                else -> Text("no books")
+            }
+        }
+
+
+
 
     }
 
